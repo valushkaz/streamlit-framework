41af58f0ee88ff61ab51b6f639340f5f68ed8c73
valushkaz <96458400+valushkaz@users.noreply.github.com>
2021-12-21 11:05:33 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"michaelc@thedataincubator.com\"\n\
+email = \"valentina.zuckerman@mg.thedataincubator.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
