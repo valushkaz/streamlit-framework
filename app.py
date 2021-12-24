399f5b35940b7478f775aad1024f05f1b3c955f5
valushkaz <96458400+valushkaz@users.noreply.github.com>
2021-12-23 17:52:23 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 import streamlit as st
 import pandas as pd
+import yfinance as yf
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+stocks = ('AAPL','ADBE','AMZN','ASML','AVGO',
+           'CMCSA','COST','CSCO','FB','GOOG','GOOGL',
+           'INTC','MSFT','NFLX','NTES','NVDA','PEP',
+           'PYPL','QCOM','TSLA')
+
+st.title('Stock dashboard for the companies with Mega market cap')
+
+stock_choice = st.multiselect('Choose the stock',stocks)
+startdate = st.date_input('Start date',value = pd.to_datetime('2001-01-01'))
+enddate = st.date_input('End date',value = pd.to_datetime('today'))
+
+
+if len(stock_choice)>0 :
+    st.write('Adjusted close price from Yahoo Finance API')
+    df=yf.download(stock_choice,startdate,enddate)['Adj Close']
+    st.line_chart(df)
+
