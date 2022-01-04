9f4c306d064ccb869a46ab175c9f98f4a5a80872
valushkaz <96458400+valushkaz@users.noreply.github.com>
2022-01-04 15:24:30 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ stocks = ('AAPL','ADBE','AMZN','ASML','AVGO',
            'INTC','MSFT','NFLX','NTES','NVDA','PEP',
            'PYPL','QCOM','TSLA')
 
-st.title('Stock dashboard for the companies with Mega market cap')
+st.title('Stock dashboard')
 
-stock_choice = st.multiselect('Choose the stock',stocks)
+stock_choice = st.multiselect('Choose the stock for the companies with Mega market cap',stocks)
+if len(stock_choice)== 0:
+    stock_choice = st.text_input ('Or you can input your own stock ticker symbol')
 startdate = st.date_input('Start date',value = pd.to_datetime('2012-01-01'))
 enddate = st.date_input('End date',value = pd.to_datetime('today'))
 
 
 if len(stock_choice)>0 :
-    st.write('Adjusted close price from Yahoo Finance API')
+    st.write('Adjusted close price')
     df=yf.download(stock_choice,startdate,enddate)['Adj Close']
     st.line_chart(df)
-
